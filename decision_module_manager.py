7211333d662d4604ef685a404f2e786d51e09882
Vitaly Klimenko <vitostikitos@gmail.com>
2024-04-25 12:18:36 +0300

    Add a declarative description of DM Manager

---
+++ decision_module_manager.py
@@ -0,0 +1,21 @@
+class DecisionModuleManager:
+    def __init__():
+        '''
+        Инициализация класса
+        '''
+        pass
+
+    def send_request_to_analyse_trend_data():
+        '''
+        Отправление сообщения DM AI об анализе тренд даты
+        '''
+
+    def send_scale_data_to_k8s():
+        '''
+        Отправление информации о скалировании в кубернетес
+        '''
+    
+    def get_event():
+        '''
+        Получение инфы из KafkaConsumer
+        '''
