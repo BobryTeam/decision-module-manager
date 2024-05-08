ec3d518b854099d1bbdcac7237de9ed07a2e863d
Vitaly Klimenko <vitostikitos@gmail.com>
2024-05-08 12:32:49 +0300

    Pass KafkaEventReader to initialization of DecisionModuleManager

--- decision_module_manager.py
+++ decision_module_manager.py
@@ -1,41 +1,46 @@
+from threading import Thread
+
+from event import *
+
+
 class DecisionModuleManager:
     '''
     Класс отвечающий за представление DM Manager
     Его задача -- отправлять запрос на анализ тренда в DM AI и отправлять запрос на скалирование в k8s
     '''
 
-    def __init__(kafka_config: KafkaConfig):
+    def __init__(self, kafka_event_reader: KafkaEventReader):
         '''
         Инициализация класса
         '''
+        self.kafka_event_reader = kafka_event_reader
+        self.running = True
+        Thread(target=self.kafka_event_reader.read_events)
+
+    async def run(self):
+        '''
+        Принятие ивентов с KafkaEventReader
+        '''
+        while self.running:
+            if self.kafka_event_reader.events.empty(): continue
+            event = self.kafka_event_reader.events.get()
+            self.handle_event(event)
+
+    # можно сделать асинк, чтобы хендлить каждый ивент в отдельном потоке    
+    def handle_event(self, event: Event):
+        '''
+        Обработка ивентов, здесь находится основная логика микросервиса
+        '''
+        match event.type:
+            case EventType.TrendData:
+                self.handle_event_trend_data(event.trend_data)
+            case EventType.Scalek8s:
+                self.handle_event_scalek8s(event.scale_data)
+            case _:
+                pass
+
+    def handle_event_trend_data(trend_data: TrendData):
         pass
 
-    def get_event(self):
-        '''
-        Считывание сообщений с кафки
-        '''
-        pass
-
-    def process_response_analyse_trend_data(self, kafka_message: str):
-        '''
-        Обработка ответа от DM AI
-        '''
-        pass
-
-    def process_response_scale_k8s(self, kafka_message: str):
-        '''
-        Обработка ответа от кубернетес
-        '''
-        pass
-
-    def send_request_to_analyse_trend_data(self):
-        '''
-        Отправление сообщения DM AI об анализе тренд даты
-        '''
-        pass
-
-    def send_request_scale_k8s(self):
-        '''
-        Отправление запроса о скалировании в кубернетес
-        '''
+    def handle_event_scalek8s(scale_data: ScaleData):
         pass
