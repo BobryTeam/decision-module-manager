5e14d2294b37284a1599e4f9fd528d759cf766c0
Vitaly Klimenko <vitostikitos@gmail.com>
2024-05-18 19:50:37 +0300

    Add TrendData and TrendAnalyseResult events handling

--- decision_module_manager.py
+++ decision_module_manager.py
@@ -1,6 +1,8 @@
 import threading
 from threading import Thread
 
+import kubernetes
+
 from events.event import *
 from events.kafka_event import *
 
@@ -17,14 +19,22 @@ class DecisionModuleManager:
     Его задача -- отправлять запрос на анализ тренда в DM AI и отправлять запрос на скалирование в k8s
     '''
 
-    def __init__(self, event_queue: Queue):
+    def __init__(self, target_deployment: str, target_namespace: str, dm_ai_event_writer: KafkaEventWriter, event_queue: Queue):
         '''
         Инициализация класса
         '''
-        self.running = threading.Event()
-        self.running.set()
 
         self.event_queue = event_queue
+
+        kubernetes.config.load_incluster_config()
+        self.target_deployment = target_deployment
+        self.target_namespace = target_namespace
+        self.k8s_api = kubernetes.client.AppsV1Api()
+
+        self.dm_ai_event_writer = dm_ai_event_writer
+
+        self.running = threading.Event()
+        self.running.set()
 
         self.running_thread = Thread(target=self.run)
         self.running_thread.start()
@@ -46,47 +56,23 @@ class DecisionModuleManager:
             case EventType.TrendData:
                 self.handle_event_trend_data(event.data)
             case EventType.TrendAnalyseResult:
-                self.handle_event_scalek8s(event.data)
+                self.handle_event_trend_analyse_result(event.data)
             case _:
                 pass
 
     def handle_event_trend_data(self, trend_data: TrendData):
         # send trend data to DM AI
-        print(f'Got trend data: {trend_data}')
+        self.dm_ai_event_writer.send_event(Event(EventType.TrendData, trend_data))
 
-    def handle_event_scalek8s(self, scale_data: ScaleData):
-        # send replica count request to k8s
-        print(f'Got scale data: {scale_data}')
+    def handle_event_trend_analyse_result(self, scale_data: ScaleData):
+        # set new replica count to the target deployment
+        deployment = self.k8s_api.read_namespaced_deployment(self.target_deployment, self.target_namespace)
+        deployment.spec.replicas = scale_data.replica_count
+        _api_response = self.k8s_api.patch_namespaced_deployment(
+            name=self.target_deployment,
+            namespace=self.target_namespace,
+            body=deployment
+        )
 
     def stop(self):
         self.running.clear()
-
-
-if __name__ == '__main__':
-    # shared event queue
-    event_queue = Queue()
-
-    kafka_event_reader = KafkaEventReader(
-        KafkaConsumer(
-            'dmm',
-            bootstrap_servers='localhost:9092',
-            auto_offset_reset='latest',
-            enable_auto_commit=True,
-        ),
-        event_queue
-    )
-
-    dmm = DecisionModuleManager(event_queue)
-
-    kafka_event_writer = KafkaEventWriter(
-        KafkaProducer(
-            bootstrap_servers='localhost:9092'
-        ),
-        'dmm'
-    )
-
-    for i in range(5):
-        kafka_event_writer.send_event(Event(EventType.TrendData, f'some bullshit {i}'))
-        time.sleep(2)
-
-    kafka_event_writer.release()
